e725e780c0484ae73ecc7624dc473c7e51fab0ca
Thomas Carney <thomas.carney@devolute.org>
2017-12-12 20:49:53 +0100

    Adds tweets

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'dotenv-rails', groups: [:development, :test]
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -53,3 +55,5 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'twitter'
--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,14 @@
 class Keyword < ApplicationRecord
+  def grab_tweets
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+
+    client.search(self.word, result_type: "recent").take(3).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
 end
